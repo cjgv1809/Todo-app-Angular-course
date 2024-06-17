6f2b05c9aba1cca2cae759b1591d61884d609f7b
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-06-17 20:35:08 -0300

    create template, styles and logic for component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from './services/tasks.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent],
+  imports: [RouterOutlet, UserComponent, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularTestApp'; // property of the component, it can be used in the template file
+
+  tasksList: string[] = [];
+  newTask: string = '';
+
+  private _tasksService = inject(TasksService);
+
+  // this method is called when the component is initialized (after the constructor)
+  ngOnInit(): void {
+    this.tasksList = this._tasksService.getTasks();
+  }
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this._tasksService.addTask(this.newTask.trim());
+      this.newTask = '';
+      this.tasksList = this._tasksService.getTasks();
+    }
+  }
+
+  removeTask(index: number) {
+    this._tasksService.removeTask(index);
+    this.tasksList = this._tasksService.getTasks();
+  }
 }
